2b76d1008615b083607c080e15131386d8de9742
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-13 12:20:43 +0000

    conversion

---
+++ 01_basics/03_convertoperation.js
@@ -0,0 +1,31 @@
+let score=33// try true, false, undefinedect
+let curr="hello123"
+
+console.log(typeof score);// number
+console.log(typeof curr);// string
+
+let valueInNumber=Number(curr)// string convert into the number
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);// it give NaN (not a number)
+console.log(typeof NaN);// number
+
+
+let run=null
+console.log(typeof null);
+let valueInNumbersss=Number(run)
+console.log(valueInNumbersss);
+
+
+// "33"= 33
+// "33aaa"=NaN
+// true=1, false=0
+
+let logIn=1
+let booleanIsLogIn=Boolean(logIn);
+console.log(booleanIsLogIn);
+
+
+
+
+
