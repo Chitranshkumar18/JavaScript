c8636e9c744a8be2b2bc9cf585fb2e5440ad3bda
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-13 14:47:59 +0000

    conversion

--- 01_basics/03_convertoperation.js
+++ 01_basics/03_convertoperation.js
@@ -1,4 +1,4 @@
-let score=33// try true, false, undefinedect
+/*let score=33// try true, false, undefinedect
 let curr="hello123"
 
 console.log(typeof score);// number
@@ -24,6 +24,50 @@ console.log(valueInNumbersss);
 let logIn=1
 let booleanIsLogIn=Boolean(logIn);
 console.log(booleanIsLogIn);
+
+*/
+//************** operation *******************//
+ let value=18
+ let negValue=-value
+ console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*5);
+// console.log(2**3);
+// console.log(2-2);
+// console.log(2/4);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Lakshay"
+let str3 = str1 + str2
+
+ console.log(str3);// both works are same 
+// console.log(str1 + str2);
+
+
+
+// isko hum use nhi karege because of it make more complex 
+console.log("1" +1 );// outpur=>11
+console.log(2 + "3" );// output=>23
+console.log(1 + 3 + "4");//output=>44
+console.log("4" + 1 + 3);// output=>
+console.log(0+"1"+2+"3"+4+"5");//output=>012345
+
+console.log(+true); // output=>1
+console.log(+"");// output=>0
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+ 
 
 
 
