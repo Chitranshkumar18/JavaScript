3c26eeb00f1aa8e176b8cacce74578d6644cd1bd
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-13 12:00:12 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,20 @@
+"use strict" // threat all js codes as newer version
+  
+let name="chitransh"
+let age=20
+let isloggedIn=false
+let state;// undefined
+let temp=null
+
+// number => 2 to power 53
+// bigint => this use if number is greater than 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log([typeof name,typeof age,typeof isloggedIn,typeof state]);
+console.log(typeof undefined)
+console.log(typeof null)// null datatype is object it act like a object
