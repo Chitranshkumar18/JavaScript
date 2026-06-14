e4b53ee94c6face8f608dfba35956253cf9a30e1
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-14 07:00:38 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,48 @@
+const score=101
+console.log(score);
+
+ const balance = new Number(100);// outpur with datatype
+ console.log(balance);
+
+console.log(typeof(balance.toString()));// its type string
+console.log(typeof balance); // its type object Because new Number(100) creates a Number object, not a primitive number.
+
+console.log(balance.toFixed(3));// it decide how much 0(zero) print after orignal number.  =>.  100.000 
+
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(5));
+
+
+
+const hund = 10000000000
+console.log(hund.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++ maths+++++++++++++++++++++++++//
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+
+
+console.log(Math.random());// randome value lie 0 to 1(0 <= random < 1) 1 kabhi nhi aayege 
+
+//if we wants to print the number in any range use random
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+
+const min = 10
+const max = 20
+
+
+// important formula
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
