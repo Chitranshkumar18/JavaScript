bcbf58fb78770ff2aee8d2b46eb4d4eb480b1bfa
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-14 07:18:58 +0000

    datatypesummary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,52 @@
+// ++++++++++++++++++    premitive       +++++++++++++++ (7) :-----
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+ const score=100// number
+ const scoreValue=100.3// number
+
+ const login=false//boolean
+ const outsidetemp=null//null
+ const str="Lakshay"//string
+ let valueLess;//undefined 
+
+ const id=Symbol('123')//Symbol
+ const anotherid= Symbol('123')//symbol
+
+console.log(id === anotherid);//false
+
+const bigNum=11118328756323247538n//Bigint
+
+
+// ++++++++++++++++++     Nonpremitive(refrence)      +++++++++++++++ (7) :-----
+ 
+// Array
+// Object
+// Function
+
+const arr=(1,2,3,4,5,6,7,8)//Array
+
+
+const myobj={// onject
+    name:"Lakshay",
+    age: 20,
+}
+console.log(myobj.name);
+
+
+
+const myFunction = function(){
+    console.log("hello word");
+    
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
