f256770d035247a09c42eb1557757ac7b7d41ebf
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-14 05:39:36 +0000

    compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+
+console.log(2>1) 
+console.log(2>=1) 
+console.log(2<1) 
+console.log(2<=1) 
+console.log(2!=1) 
+
+console.log("2">"3"); // it convert automatically string into the no
+ console.log(2>"3"); // it convert automatically string into the no
+
+console.log(0<null);//false because of null is convert into the no and that is 0(zero)
+console.log(0>null);//false because of null is convert into the no and that is 0(zero)
+console.log(0==null);//false => use diffrent rule null=== undefined
+console.log(0===null);//false => because === requires both value and type to match.
+console.log(null==undefined);// true
+
+
+console.log(typeof undefined);
+console.log(null >= 0);// true reason define above
+console.log(null <= 0);// true
+
+console.log(undefined == 0);// false =>undefined is only loosely equal to null:
+console.log(undefined > 0);//false=> Number(undefined)=>NaN
+console.log(undefined < 0);//false=> Number(undefined)=>NaN
+
+console.log("2" === 2);// false=> because === requires both value and type to
+
+
+
+
