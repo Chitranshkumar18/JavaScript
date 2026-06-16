5b2e813584d82ee5de571fefbf8e882e70f23902
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-16 10:57:44 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// iife use when we need to call immediately function
+
+
+(function code(){
+    console.log("hello i am Lakshay");
+})();
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})();
