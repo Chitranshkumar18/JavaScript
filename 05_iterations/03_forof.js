b7a199b74b34caca845600b1fbb71f12455158bb
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-17 08:01:28 +0000

    forofloop

---
+++ 05_iterations/03_forof.js
@@ -0,0 +1,40 @@
+// for of 
+
+
+
+const arr = [1,2,3,4,5,6,7,8,9]
+for (const num of arr) {
+  //  console.log(num);
+}
+
+
+const greeting = "Hello world!"
+for (const greet of greeting) {
+    //console.log(`Each char is ${greet}`);
+}
+
+
+// ++++++++maps++++++++++//
+
+const map= new Map();  // duplicate key are not allow
+map.set(1, "hello")
+map.set(2, "hello 2")
+map.set(3, "hello 3")
+map.set(4, "hello 4")
+//console.log(map);//{ 1 => 'hello', 2 => 'hello 2', 3 => 'hello 3', 4 => 'hello 4' }
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);//myObject is not iterable
+    
+// }
