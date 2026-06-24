3d7729359827ed5d1e922a4905915c5cc231e8f7
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-24 15:53:14 +0530

    for of loop

--- 05_iterations/03_forof.js
+++ 05_iterations/03_forof.js
@@ -4,13 +4,13 @@
 
 const arr = [1,2,3,4,5,6,7,8,9]
 for (const num of arr) {
-  //  console.log(num);
+    console.log(num);
 }
 
 
 const greeting = "Hello world!"
 for (const greet of greeting) {
-    //console.log(`Each char is ${greet}`);
+    console.log(`Each char is ${greet}`);
 }
 
 
@@ -23,11 +23,12 @@ map.set(3, "hello 3")
 map.set(4, "hello 4")
 //console.log(map);//{ 1 => 'hello', 2 => 'hello 2', 3 => 'hello 3', 4 => 'hello 4' }
 
-// for (const [key, value] of map) {
-//     console.log(key, ':-', value);
-// }
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
 
 
+//++++++++++object or not iterable +++++++++++++//
 
 // const myObject = {
 //     game1: 'NFS',
