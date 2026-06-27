5dd997a712d5833b3a4631f1847a0f1cb8ded610
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-27 11:21:45 +0530

    prototype

--- 09_class/02_object.js
+++ 09_class/02_object.js
@@ -1,3 +1,30 @@
+//++++++++++++++++++++++++++ PROTOTYPE   ++++++++++++++++++++++++++++//
+
+    // Prototype ek object hai jisse dusre objects properties aur methods inherit karte hain.
+   //Jab kisi object me koi property nahi milti, JavaScript uske prototype me dhoondti hai.
+   // JavaScript Class-based language nahi hai. Ye Prototype-based language hai.
+   // ES6 me class aayi, lekin internally JavaScript aaj bhi prototype hi use karti hai.
+                            //        user
+
+                            //         │
+
+                            //         ▼
+
+                            //  User.prototype
+
+                            //         │
+
+                            //         ▼
+
+                            // Object.prototype
+
+                            //         │
+
+                            //         ▼
+
+                            //        null
+
+
 function multipleBy(num){// function, function bhi hai or object bhi hai 
     return num*5
 }
