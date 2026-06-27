3ad0a81f0e52cb25cd685231c3bec19eec6bcb6a
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-06-27 10:59:58 +0530

    new keyword

--- 09_class/01_ops.js
+++ 09_class/01_ops.js
@@ -4,7 +4,8 @@ const user={
     getuser :function(){
         console.log("i am here !");
         console.log(`username: ${this.username}`);
-        
+        //JavaScript me agar function kuch return nahi karta, to wo automatically:undefined return kr deta hai
+        return(`age is:${this.age}`)// ab humne age return kr de hai isliye undefined return ni hoga 
     }
 }
 console.log(user.age);
@@ -20,6 +21,48 @@ const userOne = new User("Lakshay", 18, true)
 const userTwo = new User("CHitransh", 17, true)
 console.log(userOne);
 console.log(userTwo);
+
+
+
+//+++++++++++++++++  new    +++++++++++++++++++++++++
+//              new User("Lakshay", 18, true)
+//                             │
+//                             ▼
+// ┌──────────────────────────────────────────────┐
+// │ Step 1: Create a new empty object            │
+// │                                              │
+// │ {}                                           │
+// └──────────────────────────────────────────────┘
+//                             │
+//                             ▼
+// ┌──────────────────────────────────────────────┐
+// │ Step 2: Link the object to User.prototype    │
+// │                                              │
+// │ object.__proto__ = User.prototype            │
+// └──────────────────────────────────────────────┘
+//                             │
+//                             ▼
+// ┌──────────────────────────────────────────────┐
+// │ Step 3: Bind `this` to the new object and    │
+// │ execute the constructor function             │
+// │                                              │
+// │ this.username = "Lakshay"                    │
+// │ this.loginCount = 18                         │
+// │ this.isLoggedIn = true                       │
+// └──────────────────────────────────────────────┘
+//                             │
+//                             ▼
+// ┌──────────────────────────────────────────────┐
+// │ Step 4: Automatically return the new object  │
+// └──────────────────────────────────────────────┘
+//                             │
+//                             ▼
+//                   userOne
+//                   {
+//                     username: "Lakshay",
+//                     loginCount: 18,
+//                     isLoggedIn: true
+//                   }
 
 
 
